66f9f0cb61d655487aa4914759a19c87d74b0565
TerrenceChao <rfvbnjuyt@gmail.com>
2017-02-20 22:14:44 +0800

    Init commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = new express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send({
+        name: 'terrence',
+        likes: [
+            "movies",
+            "traveling",
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is up on port: %s', PORT);
+});
